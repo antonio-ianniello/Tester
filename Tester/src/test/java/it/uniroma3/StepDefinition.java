cde0032b2417c58086264084abf7ba7399857e1e
anton <anton@MSI>
2020-04-19 21:54:19 +0100

    ininzio progetto
    inizio progetto

---
+++ Tester/src/test/java/it/uniroma3/StepDefinition.java
@@ -0,0 +1,31 @@
+package it.uniroma3;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class StepDefinition {
+
+
+
+
+@Given("^nessuno$")
+public void nessuno() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+
+}
+
+@When("^fai$")
+public void fai() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+   }
+
+@Then("^nulla$")
+public void nulla() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+  
+}
+
+
+}
---
+++ Tester/src/test/resources/features/prova.feature
@@ -0,0 +1,10 @@
+
+@tag
+Feature: Title of your feature
+  I want to use this template for my feature file
+
+  @tag1
+  Scenario: prova
+  Given nessuno
+  When fai
+  Then nulla
